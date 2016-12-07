7dd3eb0b40da803b24ca726e9ea8319e19781c8f
andy <andyalam@hotmail.com>
2016-12-07 14:26:38 +0200

    mongodb setup and debug info

---
+++ server/api/models/db.js
@@ -0,0 +1,45 @@
+var mongoose = require('mongoose');
+
+var dbURI = 'mongodb://localhost/vocalize';
+
+mongoose.connect(dbURI);
+
+mongoose.connection.on('connected', function() {
+  console.log('Mongoose has connected to ' + dbURI);
+});
+
+mongoose.connection.on('error', function(err) {
+  console.log('Mongoose connect error: ' + err);
+});
+
+mongoose.connection.on('disconnected', function() {
+  console.log('Mongoose has disconnected');
+});
+
+gracefulShutdown = function (msg, callback) {
+  mongoose.connection.close(function () {
+    console.log('Mongoose disconnected through ' + msg);
+    callback();
+  });
+};
+
+// For nodemon restarts
+process.once('SIGUSR2', function () {
+  gracefulShutdown('nodemon restart', function () {
+    process.kill(process.pid, 'SIGUSR2');
+  });
+});
+// For normal app termination
+process.on('SIGINT', function() {
+  gracefulShutdown('app termination', function() {
+    process.exit(0);
+  });
+});
+// For heroku shutdown
+process.on('SIGTERM', function() {
+  gracefulShutdown('heroku app shutdown', function() {
+    process.exit(0);
+  });
+});
+
+require('./posts');
--- server/app.js
+++ server/app.js
@@ -5,6 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+require('./api/models/db');
+
 var api_routes = require('./api/routes/index');
 var users = require('./api/routes/users');
 
