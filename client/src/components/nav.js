a9247739eadb185ff13fea97e8d809b93677a030
andy <andyalam@hotmail.com>
2016-12-18 12:54:22 +0200

    drawer navigation working

--- client/src/components/nav.js
+++ client/src/components/nav.js
@@ -1,39 +1,51 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router';
 
 import AppBar from 'material-ui/AppBar';
-import IconMenu from 'material-ui/IconMenu';
-import MenuItem from 'material-ui/MenuItem';
+import Drawer from 'material-ui/Drawer';
 import IconButton from 'material-ui/IconButton';
-import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
+import MenuItem from 'material-ui/MenuItem';
+import NavigationClose from 'material-ui/svg-icons/navigation/close';
 
-const Dropdown = () => {
-  return (
-    <IconMenu
-      iconButtonElement={<IconButton><MoreVertIcon /></IconButton>}
-      anchorOrigin={{horizontal: 'right', vertical: 'bottom'}}
-      targetOrigin={{horizontal: 'right', vertical: 'bottom'}}
-    >
-      <MenuItem primaryText="Refresh" />
-      <MenuItem primaryText="Send feedback" />
-      <MenuItem primaryText="Settings" />
-      <MenuItem primaryText="Help" />
-      <MenuItem primaryText="Sign out" />
-    </IconMenu>
-  )
-}
+import 'style/nav';
 
 export default class Navigation extends Component {
-  componentDidMount() {
-    window.appbar = this.refs.appbar1;
+  constructor(props) {
+    super(props);
+    this.state = {open: false};
   }
+
+  handleToggle = () => this.setState({open: !this.state.open});
+
   render() {
     return (
-      <AppBar
-        title="Vocalize"
-        ref="appbar1"
-        iconElementLeft={<Dropdown />}
-        iconClassNameRight="muidocs-icon-navigation-expand-more"
-      />
+      <div className="navigation">
+        <AppBar
+          title="Vocalize"
+          iconClassNameRight="muidocs-icon-navigation-expand-more"
+          onLeftIconButtonTouchTap={this.handleToggle}
+        />
+        <Drawer open={this.state.open}>
+          <MenuItem
+            className="close-container"
+            onClick={this.handleToggle}
+          >
+            <IconButton><NavigationClose /></IconButton>
+          </MenuItem>
+          <Link
+            to={'/'}
+            onClick={this.handleToggle}
+          >
+            <MenuItem>Home</MenuItem>
+          </Link>
+          <Link
+            to={'/upload'}
+            onClick={this.handleToggle}
+          >
+            <MenuItem>Upload</MenuItem>
+          </Link>
+        </Drawer>
+      </div>
     );
   }
 }
